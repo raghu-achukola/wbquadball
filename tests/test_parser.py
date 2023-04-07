d1f6f8cd227f1461a928e48603ee43b025bb4e2e
raghu-achukola <raghu.achukola@gmail.com>
2023-04-07 14:10:45 -0500

    test reading bytes io

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,7 +1,18 @@
 from quadball.statsheet.parser import * 
 import os
+from io import BytesIO
+
 
 def test_parse_file(): 
-    file_name = os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'test_data', 'TEXAS_LSQC.xlsx'))
-    print(file_name)
+    file_name = os.path.realpath(os.path.join(os.path.dirname(__file__), 'test_data', 'TEXAS_LSQC.xlsx'))
+
     assert parse_file(file_name) == []
+
+
+def test_open_file():
+    file_name = os.path.realpath(os.path.join(os.path.dirname(__file__), 'test_data', 'hello_world.txt'))
+    output = open_file(file_name)
+
+    assert type(output) == BytesIO, f'Expected object of type BytesIO, found {type(output)}'
+    output_text = output.read().decode('utf-8')
+    assert output_text == 'Hello world!'
