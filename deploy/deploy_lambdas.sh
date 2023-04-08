daeff80d2abc95c0aac177c7eb7c77329dd5feac
raghu-achukola <raghu.achukola@gmail.com>
2023-04-08 03:57:05 -0500

    Fix incorrect aws cli code

--- deploy/deploy_lambdas.sh
+++ deploy/deploy_lambdas.sh
@@ -5,4 +5,4 @@ zip deploy_wbquadball-uw2-parse-statsheet.zip wbquadball-uw2-parse-statsheet.py
 aws s3 cp deploy_wbquadball-uw2-parse-statsheet.zip s3://wbquadball-uw2-deployment/lambdas/deploy_wbquadball-uw2-parse-statsheet.zip
 cd ..
 rm -r deploy_wbquadball-uw2-parse-statsheet
-aws lambda update-function-code --function-name wbquadball-uw2-parse-statsheet --s3-bucket wbquadball-uw2-deployment --s3-object lambdas/deploy_wbquadball-uw2-parse-statsheet.zip
+aws lambda update-function-code --function-name wbquadball-uw2-parse-statsheet --s3-bucket wbquadball-uw2-deployment --s3-key lambdas/deploy_wbquadball-uw2-parse-statsheet.zip
