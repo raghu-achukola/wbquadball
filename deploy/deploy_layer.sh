4cfcb987e3cceac423c0ff7ff4f84aaf85e7516e
raghu-achukola <raghu.achukola@gmail.com>
2023-04-09 13:13:36 -0500

    Deploye layer to s3 on merge

---
+++ deploy/deploy_layer.sh
@@ -0,0 +1,11 @@
+mkdir deploy_quadball/
+cd deploy_quadball/
+mdkir python
+cd python
+pip install -r ../../../requirements.txt -t .
+pip install ../../../ -t .
+cd ..
+zip -r deploy_quadball.zip .
+aws s3 cp deploy_quadball.zip s3://wbquadball-uw2-deployment/layers/deploy_quadball.zip
+cd ..
+rm -r deploy_quadball/
