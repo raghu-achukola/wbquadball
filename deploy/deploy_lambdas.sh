17e1198917a698ad8684be9cd2e93f0044b5b871
raghu-achukola <raghu.achukola@gmail.com>
2023-04-12 20:05:49 -0500

    It's a wonder devops engineers haven't all snapped

--- deploy/deploy_lambdas.sh
+++ deploy/deploy_lambdas.sh
@@ -1,4 +1,5 @@
 mkdir deploy_wbquadball-uw2-parse-statsheet
+echo $1
 cd deploy_wbquadball-uw2-parse-statsheet
 cp ../../lambdas/wbquadball-uw2-parse-statsheet.py .
 zip deploy_wbquadball-uw2-parse-statsheet.zip wbquadball-uw2-parse-statsheet.py
@@ -8,5 +9,4 @@ rm -r deploy_wbquadball-uw2-parse-statsheet
 aws lambda update-function-code --function-name wbquadball-uw2-parse-statsheet --s3-bucket wbquadball-uw2-deployment --s3-key lambdas/deploy_wbquadball-uw2-parse-statsheet.zip 
 # Resource conflict exception will result due to simultaneous updates unless we wait until function updated
 aws lambda wait function-updated --function-name wbquadball-uw2-parse-statsheet
-echo $1
 aws lambda update-function-configuration --function-name wbquadball-uw2-parse-statsheet --layers $1
