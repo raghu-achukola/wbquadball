6a2a3b875ce81a4620211c4bb3e54b2dddef3ff3
raghu-achukola <raghu.achukola@gmail.com>
2023-04-12 19:08:26 -0500

    Fix passing layer version to lambda

--- deploy/deploy_lambdas.sh
+++ deploy/deploy_lambdas.sh
@@ -8,5 +8,5 @@ rm -r deploy_wbquadball-uw2-parse-statsheet
 aws lambda update-function-code --function-name wbquadball-uw2-parse-statsheet --s3-bucket wbquadball-uw2-deployment --s3-key lambdas/deploy_wbquadball-uw2-parse-statsheet.zip 
 # Resource conflict exception will result due to simultaneous updates unless we wait until function updated
 aws lambda wait function-updated --function-name wbquadball-uw2-parse-statsheet
-echo $QUADBALL_LAYER
-aws lambda update-function-configuration --function-name wbquadball-uw2-parse-statsheet --layers $QUADBALL_LAYER
+echo $1
+aws lambda update-function-configuration --function-name wbquadball-uw2-parse-statsheet --layers $1
