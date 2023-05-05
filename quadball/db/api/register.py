072d1ec802f14aabc39e113ed416f20b65aee402
raghu-achukola <raghu.achukola@gmail.com>
2023-05-05 00:19:26 -0500

    Dupesafe the API

--- quadball/db/api/register.py
+++ quadball/db/api/register.py
@@ -12,13 +12,22 @@ import bson
 
 def register_league(league_collection:pymongo.collection.Collection,
                     league:League) -> bson.ObjectId: 
+    # Check if duplicate via secondary id
+    duplicate_condition = {'league_id':league.league_id}
+    if league_collection.count_documents(duplicate_condition):
+        return None
+    # If not duplicate, insert
     result = league_collection.insert_one(MessageToDict(league,preserving_proto_field_name= True))
     return result.inserted_id
 
 def register_season(season_collection:pymongo.collection.Collection,
                     season:Season 
                     ) -> bson.ObjectId:
-    
+    # Check if duplicate via secondary id
+    duplicate_condition = {'season_id':season.season_id}
+    if season_collection.count_documents(duplicate_condition):
+        return None
+    # If not duplicate, insert
     result = season_collection.insert_one(MessageToDict(season,preserving_proto_field_name= True))
     return result.inserted_id
 
@@ -31,11 +40,26 @@ def register_tournament(tournament_collection:pymongo.collection.Collection,
 
 def register_team(team_collection: pymongo.collection.Collection,
                    team:Team) -> bson.ObjectId:
+    # Check if duplicate via secondary id (Team ID + LEAGUE ID )
+    duplicate_condition = {'team_id':team.team_id,'league_id':team.league_id}
+    if team_collection.count_documents(duplicate_condition):
+        return None
+    # If not duplicate, insert
     result = team_collection.insert_one(MessageToDict(team,preserving_proto_field_name= True))
     return result.inserted_id
 
 def register_player(player_collection: pymongo.collection.Collection,
                    player:Player) -> bson.ObjectId:
+    # Check if duplicate via secondary id
+    # TODO: FIND BETTER DUPLICATE CONDITION FROM STUPID FIRST NAME + LAST NAME GOD
+    duplicate_condition = {
+        'player_first_name':player.player_first_name,
+        'player_last_name':player.player_last_name
+    }
+    
+    if player_collection.count_documents(duplicate_condition):
+        return None
+    # If not duplicate, insert    
     result = player_collection.insert_one(MessageToDict(player,preserving_proto_field_name= True))
     return result.inserted_id
 
