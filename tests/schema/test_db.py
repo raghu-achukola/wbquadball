c95174d8abeef6d60013fdc80f520d2454cb6bcb
raghu-achukola <raghu.achukola@gmail.com>
2023-05-09 20:08:10 -0500

    MongoDB Object schema validity + backcompat: Team

--- tests/schema/test_db.py
+++ tests/schema/test_db.py
@@ -4,6 +4,7 @@ import os
 from quadball.schema.db.stats_pb2 import * 
 from quadball.schema.db.league_pb2 import *
 from quadball.schema.db.season_pb2 import * 
+from quadball.schema.db.team_pb2 import * 
 
 from google.protobuf.json_format import ParseDict
 import pytest
@@ -25,6 +26,10 @@ SEASON_PATH = relpath(
 LEAGUE_PATH = relpath(
         '..','test_data', 'schema','db','league.json'
     )
+
+TEAM_PATH = relpath(
+        '..','test_data', 'schema','db','team.json'    
+)
     
 @pytest.fixture
 def load_possesions():
@@ -40,7 +45,12 @@ def load_leagues():
 def load_seasons():
     with open(SEASON_PATH) as f: 
         return json.loads(f.read())
-
+    
+@pytest.fixture
+def load_teams():
+    with open(TEAM_PATH) as f: 
+        return json.loads(f.read())
+    
 def test_db_possession(load_possesions):
     """
         Make a specific version -> eventually generalize. 
@@ -84,6 +94,21 @@ def test_db_season(load_seasons):
         season = Season()
         try: 
             ParseDict(obj,season)
+        except Exception as e: 
+            assert False, f"Parse exception occured at key {k}: {e}"
+    assert True
+
+def test_db_teams(load_teams):
+    """
+        Make a specific version -> eventually generalize. 
+        We will have "test schema" by parsing all data 
+        in schema/statsheet to Protobuf format
+    """ 
+    
+    for k, obj in load_teams.items():
+        team = Team()
+        try: 
+            ParseDict(obj,team)
         except Exception as e: 
             assert False, f"Parse exception occured at key {k}: {e}"
     assert True
