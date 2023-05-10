4997d1f232e8c7d5f24e7a933d00848d2d40832f
raghu-achukola <raghu.achukola@gmail.com>
2023-05-09 20:21:36 -0500

    MongoDB Object schema validity + backcompat: Player

--- tests/schema/test_db.py
+++ tests/schema/test_db.py
@@ -5,6 +5,7 @@ from quadball.schema.db.stats_pb2 import *
 from quadball.schema.db.league_pb2 import *
 from quadball.schema.db.season_pb2 import * 
 from quadball.schema.db.team_pb2 import *
+from quadball.schema.db.player_pb2 import *
 from quadball.schema.db.tournament_pb2 import *  
 
 from google.protobuf.json_format import ParseDict
@@ -36,6 +37,11 @@ TOURNAMENT_PATH = relpath(
         '..','test_data', 'schema','db','tournament.json'    
 
 )
+    
+PLAYER_PATH = relpath(
+        '..','test_data', 'schema','db','player.json'    
+
+)
 @pytest.fixture
 def load_possesions():
     with open(POSSESSION_PATH) as f: 
@@ -59,7 +65,11 @@ def load_teams():
 def load_tournaments():
     with open(TOURNAMENT_PATH) as f: 
         return json.loads(f.read())
-    
+@pytest.fixture
+def load_players():
+    with open(PLAYER_PATH) as f: 
+        return json.loads(f.read())
+
 def test_db_possession(load_possesions):
     """
         Make a specific version -> eventually generalize. 
@@ -74,8 +84,6 @@ def test_db_possession(load_possesions):
             assert False, f"Parse exception occured at index {i}: {e}"
     assert True
     
-
-
 def test_db_league(load_leagues):
     """
         Make a specific version -> eventually generalize. 
@@ -90,7 +98,6 @@ def test_db_league(load_leagues):
         except Exception as e: 
             assert False, f"Parse exception occured at key {k}: {e}"
     assert True
-   
     
 def test_db_season(load_seasons):
     """
@@ -133,6 +140,21 @@ def test_db_tournaments(load_tournaments):
         tourney = Tournament()
         try: 
             ParseDict(obj,tourney)
+        except Exception as e: 
+            assert False, f"Parse exception occured at key {k}: {e}"
+    assert True
+
+def test_db_players(load_players):
+    """
+        Make a specific version -> eventually generalize. 
+        We will have "test schema" by parsing all data 
+        in schema/statsheet to Protobuf format
+    """ 
+    
+    for k, obj in load_players.items():
+        player = Player()
+        try: 
+            ParseDict(obj,player)
         except Exception as e: 
             assert False, f"Parse exception occured at key {k}: {e}"
     assert True
