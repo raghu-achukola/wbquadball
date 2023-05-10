6bd70bf23aafc2f69054a7b9649fddc625d989ce
raghu-achukola <raghu.achukola@gmail.com>
2023-05-09 19:56:53 -0500

    Unit tests for MongoDB schema validity + backcompat

--- tests/schema/test_db.py
+++ tests/schema/test_db.py
@@ -2,30 +2,43 @@
 import json 
 import os
 from quadball.schema.db.stats_pb2 import * 
-from google.protobuf.json_format import ParseDict
+from quadball.schema.db.season_pb2 import * 
 
+from google.protobuf.json_format import ParseDict
+import pytest
 
 relpath = lambda *paths : os.path.relpath(
         os.path.join(
             os.path.dirname(__file__), *paths
         )
     )
-data_path = relpath(
+
+POSSESSION_PATH = relpath(
         '..','test_data', 'schema','db','stats.json'
     )
 
-                 
+SEASON_PATH = relpath(
+        '..','test_data', 'schema','db','season.json'
+    )
+    
+@pytest.fixture
+def load_possesions():
+    with open(POSSESSION_PATH) as f: 
+        return json.loads(f.read())
+    
+@pytest.fixture
+def load_seasons():
+    with open(SEASON_PATH) as f: 
+        return json.loads(f.read())
 
-def test_db_possession():
+def test_db_possession(load_possesions):
     """
         Make a specific version -> eventually generalize. 
         We will have "test schema" by parsing all data 
         in schema/statsheet to Protobuf format
     """
-    with open(data_path) as f: 
-        json_data = json.loads(f.read())
     ssp = Possession()
-    for i, obj in enumerate(json_data):
+    for i, obj in enumerate(load_possesions):
         try: 
             ParseDict(obj,ssp)
         except Exception as e: 
@@ -33,3 +46,18 @@ def test_db_possession():
     assert True
     
     
+def test_db_season(load_seasons):
+    """
+        Make a specific version -> eventually generalize. 
+        We will have "test schema" by parsing all data 
+        in schema/statsheet to Protobuf format
+    """ 
+    
+    for k, obj in load_seasons.items():
+        season = Season()
+        try: 
+            ParseDict(obj,season)
+        except Exception as e: 
+            assert False, f"Parse exception occured at key {k}: {e}"
+    assert True
+   
